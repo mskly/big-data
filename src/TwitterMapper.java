a0bbf470cd15b851baf2a4100ef1722ea41ce92a
Michael Kelly <m-kelly@outlook.com>
2015-04-08 21:27:47 -0400

    Added noun detection/counting and language filtering

--- src/TwitterMapper.java
+++ src/TwitterMapper.java
@@ -1,17 +1,51 @@
-package src;
+import java.io.InputStream;
 import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
-public class TwitterMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
-
+public class TwitterMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	
+	public void setup(Context context) {
+		try {
+			InputStream modelIn = TwitterMapper.class.getResourceAsStream("en-sent.bin");
+			SentenceModel sentenceModel = new SentenceModel(modelIn);
+			sentenceDetector = new SentenceDetectorME(sentenceModel);
+			modelIn.close();
+			
+			modelIn = TwitterMapper.class.getResourceAsStream("en-token.bin");
+			TokenizerModel tokenizerModel = new TokenizerModel(modelIn);
+			tokenizer = new TokenizerME(tokenizerModel);
+			modelIn.close();
+			
+			modelIn = TwitterMapper.class.getResourceAsStream("en-pos-maxent.bin");
+			POSModel posModel = new POSModel(modelIn);
+			tagger = new POSTaggerME(posModel);
+			modelIn.close();
+		}
+		catch (IOException e) {
+		  e.printStackTrace();
+		}
+	}
+	
+	private static SentenceDetectorME sentenceDetector;
+	private static TokenizerME tokenizer;
+	private static POSTaggerME tagger;
+	
 	private static Pattern followersPattern = Pattern.compile("\"followers_count\":([\\d]+)");
-	private static Pattern tweetTextPattern = Pattern.compile("\"text\":(.+)\"source\":");
+	private static Pattern langPattern = Pattern.compile("\"lang\":\"en\"");
+	private static Pattern tweetTextPattern = Pattern.compile("\"text\":\"([^\"]+)\"");
 
   public String removeQuotes(String text) {
 	int i;
@@ -48,29 +82,35 @@ public class TwitterMapper extends Mapper<LongWritable, Text, IntWritable, Text>
       throws IOException, InterruptedException {
 
 	  String line = value.toString();
-	  Matcher followersMatcher = followersPattern.matcher(line);
-	  Matcher tweetTextMatcher = tweetTextPattern.matcher(line);
-	  if(tweetTextMatcher.find()) {
-		String tweetText;
-		int followers;
-		tweetText = getTweet(line);
-		tweetText = removeQuotes(tweetText);  
+	  	Matcher followersMatcher = followersPattern.matcher(line);
+		Matcher languageMatcher = langPattern.matcher(line);
+		Matcher tweetTextMatcher = tweetTextPattern.matcher(line);
+		if (languageMatcher.find() && tweetTextMatcher.find() && followersMatcher.find()) {
+				int followers = Integer.parseInt(followersMatcher.group(1));
+				// I figured this was a good split between very few and very many followers
+				if(followers < 100 || followers > 10000) {
+					String tweetText = tweetTextMatcher.group(1);
+					// Convert escape sequences into normal characters
+					tweetText = StringEscapeUtils.unescapeJava(tweetText).toLowerCase();
+					
+					// Remove usernames and hashtags
+					tweetText = tweetText.replaceAll("[@#][\\w]+", " ");
 	
-		
-		if(followersMatcher.find()) { 
-	  followers = Integer.parseInt(followersMatcher.group(1));
-		  if (followers < 1000) {
-			  followers = 100 * (followers / 100);
-		  } else {
-			  followers = 1000 * (followers / 1000);
-		  }      }
-		else 
-			followers = -1;
-		  context.write(new IntWritable(followers), new Text(tweetText));
-		  }
-
+					String[] sentences = sentenceDetector.sentDetect(tweetText);
+					for (String sentence : sentences) {
+						String[] tokens = tokenizer.tokenize(sentence);
+						String[] tags = tagger.tag(tokens);
+						for (int i = 0; i < tokens.length; i++) {
+							// Only use words tagged as nouns that don't contain special characters
+							if (tags[i].matches("NNP?S?") && tokens[i].matches("[a-z]+")) {
+								context.write(new Text(tokens[i]), new IntWritable(followers));
+							}
+						}
+					}
+				}
 			}
+		}
+
+	}
 
 
-	  
-  }
--- src/TwitterReducer.java
+++ src/TwitterReducer.java
@@ -1,21 +1,28 @@
-package src;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class TwitterReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
+public class TwitterReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
   @Override
-  public void reduce(IntWritable key, Iterable<Text> values, Context context)
+  public void reduce(Text key, Iterable<IntWritable> values, Context context)
       throws IOException, InterruptedException {
 
-
-	  for (Text value : values) {
-		context.write(key, value);
+	  int countLow = 0;
+	  int countHigh = 0;
+	  for (IntWritable value : values) {
+		  if(value.get() < 100) {
+			  countLow++;
+		  } else {
+			  countHigh++;
+		  }
 	  }
-	
-
+	  if(countLow >= 10 || countHigh >= 10) {
+		  // TODO: Store the number of occurrences of each word in HBase (with different columns for the counts among low and high-follower users)?
+		  context.write(new Text(key.toString() + "_LOW"), new IntWritable(countLow));
+		  context.write(new Text(key.toString() + "_HIGH"), new IntWritable(countHigh));
+	  }
   }
 }
